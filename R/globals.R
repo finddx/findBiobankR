c5589f2d1480df55e43ac85aa1136a515e9ccdba
m-mburu <mmburu20@gmail.comm>
2023-08-04 18:30:04 +0300

    update on summary function

--- R/globals.R
+++ R/globals.R
@@ -3,4 +3,5 @@ utils::globalVariables(c("specimen_barcode",
                          "specimen_label",
                          "..nms_old_pos",
                          "checked",
+                         "..nms2",
                          "..all_cols"))
